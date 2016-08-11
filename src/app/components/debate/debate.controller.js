696f0acf224a6a701b90fc56979cf0269c91a9d1
Dmitry Tunik <dimatunik@gmail.com>
2016-08-11 18:06:05 +0300

    commit

--- src/app/components/debate/debate.controller.js
+++ src/app/components/debate/debate.controller.js
@@ -3,6 +3,12 @@ class DebateController {
     this.name = 'debate';
     this.cards = [1,2];
     this.msg = "";
+    this.recommendations = [
+      {link:"http://edition.cnn.com/2016/08/10/politics/trump-second-amendment/index.html",img:"assets/articles/test1.JPG"},
+      {link:"2",img:"dsfad"},
+      {link:"3",img:"dsfad"},
+      {link:"4",img:"dsfad"}
+    ]
   }
     
 }
